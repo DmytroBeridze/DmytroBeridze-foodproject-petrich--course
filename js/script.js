8b0f7cc4879be9c8e1221432d08b263486928a40
Dymon <moderatefast@gmail.com>
2023-12-15 23:56:28 +0200

    calculator chapter-1  Please enter the commit message for your changes. Lines starting +

--- js/script.js
+++ js/script.js
@@ -352,4 +352,75 @@ window.addEventListener("DOMContentLoaded", () => {
       currentCounter.textContent = (+counter + 1).toString().padStart(2, 0);
     }
   });
+
+  // -----------calculator
+  const calculatingResult = document.querySelector(".calculating__result span");
+  let userGender = "female",
+    userHeight,
+    userWeight,
+    userAge,
+    userActivity = 1.375;
+
+  const caloriesCalculate = () => {
+    if (
+      !userGender ||
+      !userHeight ||
+      !userWeight ||
+      !userAge ||
+      !userActivity
+    ) {
+      calculatingResult.innerHTML = "_ _";
+      return;
+    }
+    if (userGender == "male") {
+      calculatingResult.innerHTML = Math.round(
+        (88.36 + 13.4 * userWeight + 4.8 * userHeight - 5.7 * userAge) *
+          userActivity
+      );
+    } else
+      calculatingResult.innerHTML = Math.round(
+        (447.6 + 9.2 * userWeight + 3.1 * userHeight - 4.3 * userAge) *
+          userActivity
+      );
+  };
+  caloriesCalculate();
+
+  const getUserData = (selector, activeClass) => {
+    const container = document.querySelector(selector),
+      elements = document.querySelectorAll(`${selector} div`);
+    container.addEventListener("click", (e) => {
+      if (e.target !== container) {
+        if (e.target.getAttribute("data-userInfo")) {
+          userActivity = +e.target.getAttribute("data-userInfo");
+        } else userGender = e.target.getAttribute("id");
+        elements.forEach((elem) => elem.classList.remove(activeClass));
+        e.target.classList.add(activeClass);
+      }
+      caloriesCalculate();
+    });
+  };
+  getUserData("#gender", "calculating__choose-item_active");
+  getUserData(".calculating__choose_big", "calculating__choose-item_active");
+
+  const getUserConstitution = (selector) => {
+    const inputs = document.querySelectorAll(`${selector} input`);
+    inputs.forEach((elem) =>
+      elem.addEventListener("input", (e) => {
+        switch (e.target.getAttribute("id")) {
+          case "height":
+            userHeight = +e.target.value;
+            break;
+          case "weight":
+            userWeight = +e.target.value;
+            break;
+          case "age":
+            userAge = +e.target.value;
+            break;
+        }
+        caloriesCalculate();
+      })
+    );
+  };
+
+  getUserConstitution(".calculating__choose_medium");
 });
